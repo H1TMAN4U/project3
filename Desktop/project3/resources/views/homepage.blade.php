72568662a11c8a3ea34ca6bae58d97732c5504d6
H1TMAN4U <andris.e.artejevs@gmail.com>
2022-11-17 09:30:32 +0200

    added nav bar

--- Desktop/project3/resources/views/homepage.blade.php
+++ Desktop/project3/resources/views/homepage.blade.php
@@ -1,78 +1,123 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSS only -->
-    <script src="https://cdn.tailwindcss.com"></script>    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!-- CSS only -->
+<script src="https://cdn.tailwindcss.com"></script>    <title>Document</title>
 </head>
+<style>
+  .intro{
+    background-image: url(https://mykaleidoscope.ru/en/uploads/posts/2022-09/1663731150_1-mykaleidoscope-ru-p-food-on-a-black-background-yeda-oboi-1.jpg);
+    background-repeat: no-repeat;
+    max-height: 100vh;;
+    background-size: 100%;
+    height: 500vh;
+    top: 0;
+  }
+  .intro h1{
+    color: #fff;
+  }
+  .intro button{
+    padding: 0 70px 0 70px;
+    transition: 0.5s;
+  }
+</style>
 <body>
-    <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <section class="intro">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 bg-white-400 flex justify-center justify-items-center" style="display: flex;
-                flex-direction: column;">
-                    <section>
-                        
-                    </section>
-                    <section class="overflow-hidden text-gray-700">
-                        <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
-                          <div class="flex flex-wrap -m-1 md:-m-2">
-                            <div class="flex flex-wrap w-1/2">
-                              <div class="w-1/2 p-1 md:p-2">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp">
-                              </div>
-                              <div class="w-1/2 p-1 md:p-2">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp">
-                              </div>
-                              <div class="w-full p-1 md:p-2">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
-                              </div>
-                            </div>
-                            <div class="flex flex-wrap w-1/2">
-                              <div class="w-full p-1 md:p-2">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp">
-                              </div>
-                              <div class="w-1/2 p-1 md:p-2 rounded-t-lg">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp">
-                              </div>
-                              <div class="w-1/2 p-1 md:p-2 rounded-t-lg">
-                                <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
-                                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp">
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-
-                      </section>
-                      <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
-                        <button class="bg-neutral-400 rounded-lg" style="width: 100%; height: 100px;">
-                            <h1 style="font-size: 32px;font-weight: 600;">More</h1>
-                        </button>
-                      </div>
-                      
-
-                </div>
-            </div>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+      <div class="container flex flex-wrap items-center justify-between mx-auto">
+        <a href="https://flowbite.com/" class="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            </li>
+          </ul>
         </div>
-    </div>
+      </div>
+    </nav>
     
-</x-app-layout>
+    <div class="container mx-auto items-center flex  ">
+      <div class="flex justify-center column ">
+        <div class="flex justify-items-center items-center content-center flex-col">
+          <h1 class="text-5xl font-normal leading-normal text-center p-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, aspernatur?</h1>
+          <button class="border-4 border-red-600 hover:bg-red-600">
+            <h1 style="font-size: 32px;font-weight: 600;">More</h1>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+<div class="py-12">
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class="p-6 bg-white border-b border-gray-200 bg-white-400 flex justify-center justify-items-center" style="display: flex; flex-direction: column;">
+
+        <section class="overflow-hidden text-gray-700">
+          <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+            <div class="flex flex-wrap -m-1 md:-m-2">
+              <div class="flex flex-wrap w-1/2">
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp">
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp">
+                </div>
+                <div class="w-full p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp">
+                </div>
+              </div>
+              <div class="flex flex-wrap w-1/2">
+                <div class="w-full p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp">
+                </div>
+                <div class="w-1/2 p-1 md:p-2 rounded-t-lg">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp">
+                </div>
+                <div class="w-1/2 p-1 md:p-2 rounded-t-lg">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp">
+                </div>
+              </div>
+            </div>
+            <div class="py-2">
+              <button class="bg-neutral-400 rounded-lg " style="width: 100%; height: 100px;">
+                <h1 style="font-size: 32px;font-weight: 600;">More</h1>
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
